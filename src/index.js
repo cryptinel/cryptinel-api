520c281206f886a5ea1ad8a52dfb9746e92298bb
Bruno Peixoto <brunolnetto@gmail.com>
2022-04-02 13:40:23 -0300

    misc/ main feature exposed

--- src/index.js
+++ src/index.js
@@ -4,10 +4,23 @@ import express from 'express';
 import 'lodash.combinations';
 import _ from 'lodash';
 
+import date from 'date-and-time';
+
+import {
+	onCurrencies,
+	getCurrencies,
+	logCurrenciesCallback,
+} from './controllers/currencies.js'
+
 import {
 	onCurrencyRates,
+	onCurrencyRatesHistory,
 	saveCurrencyRatesCallback,
 } from './controllers/exchangeRate.js'
+
+import {
+	today,
+} from './utils/time.js'
 
 const app = express();
 
@@ -24,10 +37,9 @@ app.listen(PORT, () => {
 });
 
 // Driver program - Create a sample graph
-app.get('/', (req, res) => {
-	//onCurrencies(saveCurrencies)
-	onCurrencyRates('brl', saveCurrencyRatesCallback)
-	
+app.get('/', async (req, res) => {
+	onCurrencyRatesHistory('eur', 'brl', '2022-01-01', '2022-04-01', console.log)
+		
 	res.send('Hi!')
 });
 // [END app]
