5e049559feb4d81264a6b2f74c308cc8b4501339
Bruno Peixoto <brunolnetto@gmail.com>
2022-04-02 13:21:32 -0300

    refac/ axios reviewed with callback and on premise async returns

--- src/axios/axios_broker.js
+++ src/axios/axios_broker.js
@@ -1,22 +1,22 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest';
+const BASE_URL = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1';
 
 const getRequest = async (url, callback) => {
-  axios
+  return await axios
     .get(url, {})
     .then((res) => {
-      callback(res);
+      return callback(res);
     })
     .catch((error) => {
       console.error(error);
     });
 };
 
-export const getCurrenciesAsync = async (callback) => {
-  getRequest(`${BASE_URL}/currencies.json`, callback);
+export const CurrenciesAsync = async (callback, date_ = 'latest') => {
+  return await getRequest(`${BASE_URL}/${date_}/currencies.json`, callback);
 };
 
-export const getCurrencyRatesAsync = async (base_currency, callback) => {
-  getRequest(`${BASE_URL}/currencies/${base_currency}.json`, callback);
+export const CurrencyRatesAsync = async (base_currency, callback, date_ = 'latest') => {
+  return await getRequest(`${BASE_URL}/${date_}/currencies/${base_currency}.json`, callback);
 };
