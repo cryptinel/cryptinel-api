acdbbe919689492cb6f649cf123cf14263c70466
Bruno Peixoto <brunolnetto@gmail.com>
2022-04-03 17:27:15 -0300

    feat/ median measure

---
+++ src/utils/math.js
@@ -0,0 +1,14 @@
+export const median = (values) => {
+    if(values.length ===0) throw new Error("No inputs");
+  
+    values.sort(function(a,b){
+      return a-b;
+    });
+  
+    var half = Math.floor(values.length / 2);
+    
+    if (values.length % 2)
+      return values[half];
+    
+    return (values[half - 1] + values[half]) / 2.0;
+  }
