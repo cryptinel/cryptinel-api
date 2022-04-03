41c235f8f057e97138c571fa628132f71a75ccdd
Bruno Peixoto <brunolnetto@gmail.com>
2022-04-03 19:00:05 -0300

    feat/ available crypto currencies

--- src/utils/currency.js
+++ src/utils/currency.js
@@ -6,3 +6,13 @@ export const extractHeaderDate = (payload) => {
 	return dateStringToFilename(`${payload.headers['date']}`);
 }
 
+export const getCryptoSymbols = () => {
+	return [
+		'btc', 'eth', 'wbtc', 'usdt', 'bnb', 'sol', 
+		'xrp', 'ada', 'avax', 'luna', 'dot', 'busd', 
+		'shib', 'matic', 'cro', 'dai', 'atom', 'ltc', 
+		'link', 'uni', 'brx', 'bch', 'ftt', 'etc', 'algo', 
+		'xlm', 'vet', 'icp', 'fil', 'egld', 'theta', 'xmr', 
+		'grt', 'one', 'ksm', 'chz'
+	]
+}
